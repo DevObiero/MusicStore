a63f48d4874d0e9b84115105d84d17543577a8b3
George <georgeobiero2@gmail.com>
2016-02-22 15:53:46 +0300

    Untrack .iml file, add author info

--- src/main/java/com/emusicstore/controller/HomeController.java
+++ src/main/java/com/emusicstore/controller/HomeController.java
@@ -3,6 +3,10 @@ package com.emusicstore.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Controller for the homepage of the application.
+ * @author George Obiero
+ */
 @Controller
 public class HomeController {
     @RequestMapping("/")
