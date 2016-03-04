a65756167b229c9541e13abc48751dbab1ac7dfc
George <georgeobiero2@gmail.com>
2016-03-04 15:58:47 +0300

    added spring basic security

--- src/main/java/com/emusicstore/controller/HomeController.java
+++ src/main/java/com/emusicstore/controller/HomeController.java
@@ -27,7 +27,6 @@ import java.util.List;
  */
 @Controller
 public class HomeController {
-    private Path path;
     @Autowired
     private ProductDao productDao;
     @RequestMapping("/")
@@ -47,63 +46,5 @@ public class HomeController {
         model.addAttribute(product);
         return "viewProduct";
     }
-    @RequestMapping("/admin")
-    public String adminPage() {
-        return "admin";
-    }
-    @RequestMapping("/admin/productInventory")
-    public String productInventory(Model model) {
-        List<Product> products = productDao.getAllProducts();
-        model.addAttribute("products", products);
-        return "productInventory";
-    }
-    @RequestMapping("/admin/productInventory/addProduct")
-    public String addProduct(Model model) {
-        Product product = new Product();
-        product.setProductCategory("instrument");
-        product.setProductCondition("active");
-        product.setProductStatus("New");
-        product.setProductDescription("this is a test description");
-        model.addAttribute("product", product);
-        return "addProduct";
-    }
-    @RequestMapping(value = "/admin/productInventory/addProduct", method = RequestMethod.POST)
-    public String addProductPost(@Valid @ModelAttribute("product") Product product, BindingResult result, HttpServletRequest request) {
-        // check for errors
-        if(result.hasErrors()) {
-            return "addProduct";
-        }
-        productDao.addProduct(product);
-        MultipartFile productImage = product.getProductImage();
-        String rootDirectory = request.getSession().getServletContext().getRealPath("/");
-        path = Paths.get(rootDirectory + "/WEB-INF/resources/images/"+product.getProductId()+".png");
 
-        if (productImage != null && !productImage.isEmpty()) {
-            try {
-                productImage.transferTo(new File(path.toString()));
-            } catch (Exception e) {
-                e.printStackTrace();
-                throw new RuntimeException("Product image saving failed", e);
-            }
-        }
-
-        return "redirect:/admin/productInventory";
-    }
-    @RequestMapping("/admin/productInventory/deleteProduct/{id}")
-    public String deleteProduct(@PathVariable int id, Model model, HttpServletRequest request) {
-        String rootDirectory = request.getSession().getServletContext().getRealPath("/");
-        path = Paths.get(rootDirectory + "/WEB-INF/resources/images/"+id+".png");
-
-        if(Files.exists(path)){
-            try {
-                Files.delete(path);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        }
-
-        productDao.deleteProduct(id);
-        return "redirect:/admin/productInventory";
-    }
 }
